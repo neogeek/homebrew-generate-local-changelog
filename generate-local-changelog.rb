fb3688c9c5cc8a008cd0c33039babf1f4cbd97fa
Scott Doxey <neogeek0101@gmail.com>
2021-02-04 01:48:53 -0500

    Version bump.

--- generate-local-changelog.rb
+++ generate-local-changelog.rb
@@ -1,8 +1,8 @@
 class GenerateLocalChangelog < Formula
   desc "Generate a CHANGELOG for your project using only local git history. No internet connection or git server API is required."
   homepage "https://github.com/neogeek/generate-local-changelog"
-  url "https://github.com/neogeek/generate-local-changelog/archive/v1.0.2.tar.gz"
-  sha256 "5cebd30283fe1b5e553448f516fc7976593510fe915462d2284419be2083f8c3"
+  url "https://github.com/neogeek/generate-local-changelog/archive/v1.0.3.tar.gz"
+  sha256 "f05143e80b12e7fc5f5ecbfc0c37043c17d6be804541d9ce0930b852c81c2907"
 
   def install
     bin.install "bin/generate-local-changelog"
