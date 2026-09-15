2ce44870692c6e048e1a8a8631cb2f9c8e6c4202
Scott Doxey <neogeek0101@gmail.com>
2026-09-15 01:47:22 -0400

    Version bump.
    Updated formula to work with the latest project setup.

--- generate-local-changelog.rb
+++ generate-local-changelog.rb
@@ -1,11 +1,31 @@
 class GenerateLocalChangelog < Formula
-  desc "Generate a CHANGELOG for your project using only local git history. No internet connection or git server API is required."
-  homepage "https://github.com/neogeek/generate-local-changelog"
-  url "https://github.com/neogeek/generate-local-changelog/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "f798a4347f32ece1f8e2709f6776f6e1305f856376cceec693a23fa4199ffeb6"
+  version '2.0.1'
+  desc 'Generate a CHANGELOG for your project using only local git history. No internet connection or git server API is required.'
+  homepage 'https://github.com/neogeek/generate-local-changelog'
 
-  def install
-    bin.install "bin/generate-local-changelog"
+  on_macos do
+    if Hardware::CPU.intel?
+      url 'https://github.com/neogeek/generate-local-changelog/raw/refs/tags/v2.0.1/bin/macos/x86_64/generate-local-changelog'
+      sha256 '2462db725313cb2e30a6873a23ef69ccc3f691659de711cdfa92061b38ea3b12'
+    elsif Hardware::CPU.arm?
+      url 'https://github.com/neogeek/generate-local-changelog/raw/refs/tags/v2.0.1/bin/macos/aarch64/generate-local-changelog'
+      sha256 '85b557c63b7af3466052233b3662632a1253d9ea3561ad0401efd2214296a8c1'
+    end
   end
 
+  on_linux do
+    url 'https://github.com/neogeek/generate-local-changelog/raw/refs/tags/v2.0.1/bin/linux/generate-local-changelog'
+    sha256 '469de7ed1a19983a4628530afe6aa78d3303aa67d48674a6a6679cea2b1a8927'
+  end
+
+
+  def install
+    bin.install 'generate-local-changelog'
+
+    chmod 0755, bin/'generate-local-changelog'
+  end
+
+  test do
+    assert_match 'generate-local-changelog', shell_output("#{bin}/generate-local-changelog --help")
+  end
 end
